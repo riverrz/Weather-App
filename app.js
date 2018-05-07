98118dd3fc2eb7938a96aa809b6e4b0e977cc9c5
shivam123425 <shivam123425@gmail.com>
2018-05-07 18:39:39 +0530

    Add error handling for fetching the result

--- app.js
+++ app.js
@@ -19,11 +19,18 @@ const argv = yargs
 request({
     url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURIComponent(argv.address)}`,
     json: true
-},(err,response,body)=> {
-    if(err || response.statusCode!==200) {
-        console.log("Error ");
+},(error,response,body)=> {
+    if(error) {
+        console.log("Unable to connect to Google servers ");
+    }else if(body.status==="ZERO_RESULTS") {
+        console.log("Unable to find that address");
+    }else if(body.status==="OK") {
+        console.log(`Address: ${body.results[0].formatted_address}`);
+        console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
+        console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
+    } else {
+        console.log("Try again later");
+        
     }
-    console.log(`Address: ${body.results[0].formatted_address}`);
-    console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
-    console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
+    
 });
