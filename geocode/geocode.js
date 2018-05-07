11300848b67ba8dc9936caa53b5edc4a57fb4ae1
shivam123425 <shivam123425@gmail.com>
2018-05-07 19:49:46 +0530

    Dried app.js and geocode.js

--- geocode/geocode.js
+++ geocode/geocode.js
@@ -1,20 +1,22 @@
 const request = require("request");
 
-const geocodeAddress = (address)=> {
+const geocodeAddress = (address,callback)=> {
     request({
         url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURIComponent(address)}`,
         json: true
     }, (error, response, body) => {
         if (error) {
-            console.log("Unable to connect to Google servers ");
+            callback("Unable to connect to Google servers ");
         } else if (body.status === "ZERO_RESULTS") {
-            console.log("Unable to find that address");
+            callback("Unable to find that address");
         } else if (body.status === "OK") {
-            console.log(`Address: ${body.results[0].formatted_address}`);
-            console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
-            console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
+            callback(undefined, {
+                address: body.results[0].formatted_address,
+                latitude: body.results[0].geometry.location.lat,
+                longitude: body.results[0].geometry.location.lng
+            });
         } else {
-            console.log("Try again later");
+            callback("Try again later");
         }
     });
 }
